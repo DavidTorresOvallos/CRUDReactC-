4d52b9368ac1062137b80179c254e6bb22882d16
DavidTorresOvallos <davidtorresovallos@gmail.com>
2023-08-15 21:12:36 -0500

    Basic Controller

---
+++ CRUD/Controllers/ContactController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly DbpruebaContext _dbContext;
+
+        public ContactController(DbpruebaContext context)
+        {
+            this._dbContext = context;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> Lista()
+        {
+            List<Contact> lista = await _dbContext.Contacts.OrderByDescending(c => c.IdContact).ToListAsync();
+            return StatusCode(StatusCodes.Status200OK, lista);
+        }
+
+        [HttpPost]
+        [Route("Guardar")]
+        public async Task<IActionResult> Guardar([FromBody] Contact request)
+        {
+            await _dbContext.Contacts.AddAsync(request);
+            await _dbContext.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status200OK, "OK");
+        }
+
+        [HttpPut]
+        [Route("Editar")]
+        public async Task<IActionResult> Editar([FromBody,] Contact request)
+        {
+            _dbContext.Contacts.Update(request);
+            await _dbContext.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status200OK, "OK");
+        }
+
+        [HttpDelete]
+        [Route("Eliminar/{id:int}")]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            Contact contact = _dbContext.Contacts.Find(id);
+
+            _dbContext.Contacts.Remove(contact);
+            await _dbContext.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status200OK,"OK");
+        }
+    }
+}
---
+++ CRUD/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUD.Models;
+
+public partial class Contact
+{
+    public int IdContact { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Correo { get; set; }
+
+    public string? Telefono { get; set; }
+}
